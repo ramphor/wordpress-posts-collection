672b7774453db6ff3a193f9d11bfe29cffd997b5
Puleeno Nguyen <puleeno@gmail.com>
2021-05-26 10:23:38 +0700

    Follow new jankx/template structure

--- src/CollectionTemplate.php
+++ src/CollectionTemplate.php
@@ -5,19 +5,20 @@ use Jankx\Template\Template;
 
 class CollectionTemplate
 {
-    protected static $loader;
+    protected static $engine;
 
-    protected static function getLoader()
+    protected static function getEngine()
     {
-        if (is_null(static::$loader)) {
+        if (is_null(static::$engine)) {
             $collectionTemplateDir = sprintf('%s/templates', constant('RAMPHOR_COLLECTION_ROOT_DIR'));
-            static::$loader = Template::getLoader(
-                $collectionTemplateDir,
+            static::$engine = Template::createEngine(
+                'ramphor_collection',
                 apply_filters('ramphor_collection_theme_directory_name', 'templates/collection'),
+                $collectionTemplateDir,
                 'wordpress'
             );
         }
-        return static::$loader;
+        return static::$engine;
     }
 
     /**
@@ -32,7 +33,7 @@ class CollectionTemplate
         $args = func_get_args();
 
         return call_user_func_array(array(
-            static::getLoader(),
+            static::getEngine(),
             'render'
         ), $args);
     }
